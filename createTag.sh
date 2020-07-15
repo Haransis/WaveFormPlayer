0fe9b4cb35766c1b0634258d99c6ea1ade87c497
Haran <ara.rahier@gmail.com>
2020-07-15 18:00:54 +0200

    :rocket: Create script for release automation

---
+++ createTag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+previousVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+echo "Which version number will you choose for a new release ?"
+read -r newVersion
+echo "$newVersion"
+echo "$previousVersion"
+sed -i "s/$previousVersion/$newVersion/" README.md
+sed -i "/versionName/s/$previousVersion/$newVersion/" soundwave/build.gradle
+code=$(grep versionCode < soundwave/build.gradle | sed 's/.* //g')
+newCode="$((code+1))"
+sed -i "/versionCode/s/$code/$newCode/" soundwave/build.gradle
+git add README.md soundwave/build.gradle
+git commit -m ":pencil: Update version number"
+git tag -a "$newVersion"

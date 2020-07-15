59fe4805da043648a6f2cd5d66d0c28f50d6b422
Haran <ara.rahier@gmail.com>
2020-07-15 18:04:19 +0200

    :fire: Remove useless echo from script

--- createTag.sh
+++ createTag.sh
@@ -3,8 +3,6 @@
 previousVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 echo "Which version number will you choose for a new release ?"
 read -r newVersion
-echo "$newVersion"
-echo "$previousVersion"
 sed -i "s/$previousVersion/$newVersion/" README.md
 sed -i "/versionName/s/$previousVersion/$newVersion/" soundwave/build.gradle
 code=$(grep versionCode < soundwave/build.gradle | sed 's/.* //g')

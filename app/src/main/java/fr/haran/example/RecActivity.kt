3e5d63c03d6d23f707604a7e1c2fc2d903001f6c
Haran <ara.rahier@gmail.com>
2020-08-04 16:21:15 +0200

    :alembic: Provide a working example with SurfaceView

--- app/src/main/java/fr/haran/example/RecActivity.kt
+++ app/src/main/java/fr/haran/example/RecActivity.kt
@@ -3,17 +3,30 @@ package fr.haran.example
 import android.Manifest
 import android.content.pm.PackageManager
 import android.icu.text.SimpleDateFormat
+import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.KeyEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import fr.haran.soundwave.ui.RecView
+import fr.haran.soundwave.ui.*
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.*
+import kotlin.experimental.and
+import kotlin.properties.Delegates
+
+private const val RECORDER_SAMPLERATE = 44100
+private const val RECORDER_CHANNELS: Int = android.media.AudioFormat.CHANNEL_IN_MONO
+private const val RECORDER_AUDIO_ENCODING: Int = android.media.AudioFormat.ENCODING_PCM_16BIT
+private const val BUFFER_ELEMENT_TO_REC = 1024 // want to play 2048 (2K) since 2 bytes we use only 1024
+private const val BYTES_PER_ELEMENT = 2 // 2 bytes in 16bit format
 
 private const val PERMISSION_CODE = 0
 private const val PERMISSION_RECORD_AUDIO = Manifest.permission.RECORD_AUDIO
@@ -24,8 +37,10 @@ class RecActivity : AppCompatActivity() {
     private lateinit var runnable: Runnable
     private val handler = Handler()
 
-    private var recorder: MediaRecorder? = null
+    private var recorder: AudioRecord? = null
+    private var recordingThread: Thread? = null
     private var isRecording = false
+    private var bufferSize by Delegates.notNull<Int>()
 
     private lateinit var recView: RecView
 
@@ -34,10 +49,24 @@ class RecActivity : AppCompatActivity() {
         setContentView(R.layout.activity_rec)
         recView = findViewById(R.id.recview)
         setOnClickListeners()
+        runnable = object: Runnable {
+            override fun run() {
+                val sData = ShortArray(bufferSize)
+                recorder!!.read(sData, 0, bufferSize)
+                recView.addAmplitude(sData.average().toInt())
+
+                if (isRecording)
+                    handler.postDelayed(this, INTERVAL)
+            }
+        }
+        bufferSize = AudioRecord.getMinBufferSize(
+            RECORDER_SAMPLERATE,
+            RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING
+        )
     }
 
     private fun setOnClickListeners(){
-        val rec = findViewById<FloatingActionButton>(R.id.rec).apply{
+        findViewById<FloatingActionButton>(R.id.rec).apply{
             setOnClickListener {
                 if (isRecording){
                     stopRecording()
@@ -71,38 +100,75 @@ class RecActivity : AppCompatActivity() {
     }
 
     private fun startRecording() {
-        prepareRecorder()
-        recorder!!.start()
-        handler.post(runnable)
+        recorder = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+            RECORDER_AUDIO_ENCODING, bufferSize * BYTES_PER_ELEMENT
+        )
+        recorder!!.startRecording()
         isRecording = true
+        recordingThread = Thread(Runnable { writeAudioDataToFile() }, "AudioRecorder Thread")
+        recordingThread!!.start()
+        handler.post(runnable)
     }
 
-    private fun prepareRecorder() {
-        val storageDir = applicationContext.externalCacheDir?.absolutePath
-        //val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.FRANCE).format(Date())
-        val name: String = "recording"
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile(storageDir + File.separator + "3GP_${name}_" + ".3gp")
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-            prepare()
+    private fun writeAudioDataToFile() {
+        // Write the output audio in byte
+        val filePath = applicationContext.externalCacheDir?.absolutePath + "/voice8K16bitmono.pcm"
+        val sData = ByteArray(bufferSize)
+        var os: FileOutputStream? = null
+        try {
+            os = FileOutputStream(filePath)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
         }
-        runnable = object: Runnable {
-            override fun run() {
-                recView.addAmplitude(recorder!!.maxAmplitude)
-                if (isRecording)
-                    handler.postDelayed(this, INTERVAL)
+        while (isRecording) {
+            recorder!!.read(sData, 0, bufferSize)
+            try {
+                // writes the data to file from buffer
+                os?.write(sData, 0, bufferSize)
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
+        try {
+            os?.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun shortToByte(sData: ShortArray): ByteArray? {
+        val shortArrsize = sData.size
+        val bytes = ByteArray(shortArrsize * 2)
+        for (i in 0 until shortArrsize) {
+            bytes[i * 2] = (sData[i] and 0x00FF).toByte()
+            bytes[i * 2 + 1] = (sData[i].toInt() shr 8).toByte()
+            sData[i] = 0
+        }
+        return bytes
     }
 
     private fun stopRecording() {
-        recorder?.stop()
-        recorder?.reset()
-        recorder?.release()
-        recorder = null
+        recorder?.let {
+            isRecording = false
+            it.stop()
+            it.release()
+            recorder = null
+            recordingThread = null
+        }
         handler.removeCallbacks(runnable)
-        isRecording = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish()
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
